d9053aa4b527a42c71e8cc84256127cb8095485a
Bill Jetzer <jetzerb@sva.com>
2020-04-11 19:18:44 -0500

    Base: Add tests, misc fixes
    Add testing script for the software installed in the base image.
    Also remove some cruft from the container startup script.
    Adjust the Dockerfile to use ARG for manifest file location, and the
    install script to recognize and use it.

---
+++ base/run_tests.sh
@@ -0,0 +1,191 @@
+#!/usr/bin/env bash
+
+#
+# Test Script for Base layer
+#
+
+
+#
+# Function to spit out a header for the output of a test
+printHdr () {
+	echo -e "\nCommand: $1" |sed 'h; s/./-/g; p; x;';
+}
+
+checkInstall() {
+	printHdr "$1";
+	dpkg -l "$1" \
+	| awk '
+		BEGIN {msg = "ERROR: Package Not Installed!";}
+		/^ii/ {msg = "OK";}
+		END {print msg;}';
+}
+
+checkCommand() {
+	printHdr "$1";
+	eval "$2"  > /dev/null && echo "OK" || echo "ERROR";
+}
+
+# ensure we're all set up properly
+. /etc/profile;
+
+dir=/tmp/test;
+mkdir -p "$dir" && cd "$dir";
+
+# sample CSV file used in multiple tests
+csv=test.csv;
+echo -e 'a,b,c,d\n1,2,3,4' > "$csv";
+
+
+
+
+
+# Test everything installed in the Dockerfile
+
+# packages installed in layer 1
+# for some packages without real commands, just verify package is installed
+checkInstall "tzdata";
+checkInstall "libxtst6";
+checkInstall "locales";
+
+checkCommand 'openssh-server' 'sshd -t';
+
+checkInstall "xauth";
+
+checkInstall "ca-certificates";
+
+checkCommand 'gnupg' 'gpg --gen-random 1 2';
+
+cmd="rsync"; checkCommand "$cmd" '$cmd --list-only $csv';
+
+checkCommand 'dbus-x11' 'dbus-launch';
+
+checkInstall 'at-spi2-core';
+
+# not sure how to make yank-cli not require keyboard input during actual usage
+checkCommand 'yank' 'yank-cli -v';
+
+checkInstall "xclip";
+
+cmd='httping'; checkCommand "$cmd" '$cmd -c 1 -l https://github.com';
+
+cmd='ping'; checkCommand "$cmd" '$cmd -c 1 github.com';
+
+cmd='busybox'; checkCommand "$cmd" '$cmd echo "foo"';
+
+cmd='less'; checkCommand "$cmd" 'echo "foo" |$cmd -F';
+
+# can't really check an individual command from because if it's
+# not installed, they'd probably succeed due to busybox and/or toybox
+checkInstall 'coreutils';
+
+cmd='tree'; checkCommand "$cmd" '$cmd .';
+
+# can't really check an individual command from because if it's
+# not installed, they'd probably succeed due to busybox and/or toybox
+checkInstall 'diffutils';
+
+cmd='colordiff'; checkCommand "$cmd" '$cmd <(echo "foo") <(echo "foo")';
+
+checkInstall 'bash';
+checkInstall 'bash-completion';
+
+cmd='perl'; checkCommand "$cmd" '$cmd -e "print;"';
+
+cmd='zip'; checkCommand "$cmd" 'echo "foo" |$cmd -q';
+
+checkInstall 'neovim';
+
+cmd='tmux'; checkCommand "$cmd" '$cmd -c exit';
+
+checkInstall 'ncurses-term';
+
+cmd='sudo'; checkCommand "$cmd" '$cmd echo';
+
+xslt='
+<xsl:stylesheet
+     xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+     version="1.0">
+	<xsl:template match="*">
+		<xsl:copy/>
+	</xsl:template>
+</xsl:stylesheet>';
+xml='<a>foo</a>';
+cmd='xsltproc'; checkCommand "$cmd" '$cmd <(echo "$xslt") <(echo "<a>foo</a>")';
+
+json='{"a":1}';
+cmd='jq'; checkCommand "$cmd" 'echo "$json" | jq "."';
+
+cmd='wget'; checkCommand "$cmd" '$cmd -q --spider https://github.com';
+
+cmd='curl'; checkCommand "$cmd" '$cmd -s https://github.com';
+
+checkInstall 'meld'; # headless system; no GUI
+
+cmd='parallel'; checkCommand "$cmd" '$cmd echo ::: A B C';
+
+cmd='ministat'; checkCommand "$cmd" 'echo -e "1 2\n3 4\n5 6" | $cmd';
+
+checkCommand 'imagemagick' 'convert -size 1x1 canvas:white null:';
+
+cmd='gnuplot'; checkCommand "$cmd" 'echo -e "set term eps\nplot x" | $cmd';
+
+pdf='test.pdf';
+# short but valid PDF file, courtesy of https://stackoverflow.com/a/17280876
+cat <<'EOF' > "$pdf"
+%PDF-1.0
+1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj 2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj 3 0 obj<</Type/Page/MediaBox[0 0 3 3]>>endobj
+trailer<</Size 4/Root 1 0 R>>
+EOF
+checkCommand 'poppler-utils' 'pdfinfo $pdf';
+
+cmd='ddgr'; checkCommand "$cmd" '$cmd --noprompt $cmd';
+
+# don't know how to test interactively
+cmd='nnn'; checkCommand "$cmd" '$cmd -V';
+
+cmd='lynx'; checkCommand "$cmd" '$cmd --dump https://duckduckgo.com';
+
+cmd='sc'; checkCommand "$cmd" 'echo "let A0 = 1" | $cmd -P %';
+
+checkInstall 'rxvt-unicode'; # headless system; no GUI
+
+# no command, just files
+checkInstall 'fonts-dejavu';
+
+# **** xpdf not available in 20.04 yet checkInstall 'xpdf'; # headless system; no GUI
+
+
+
+
+# software installed in layer 2
+
+cmd='toybox';  checkCommand "$cmd" '$cmd echo';
+cmd='bitwise'; checkCommand "$cmd" '$cmd 123';
+cmd='ripgrep'; checkCommand "$cmd" 'rg a $csv';
+cmd='xsv';     checkCommand "$cmd" '$cmd select b $csv';
+
+json='{"foo": {"bar":"baz"}, "num":123}';
+cmd="gron";    checkCommand "$cmd" 'echo $json | $cmd';
+
+cmd="trdsql";  checkCommand "$cmd" '$cmd -driver sqlite3 -icsv -ih -oat "select c,b from $csv"';
+
+cmd="bat";     checkCommand "$cmd" '$cmd $csv';
+cmd="fd";      checkCommand "$cmd" '$cmd $csv';
+cmd="hexyl";   checkCommand "$cmd" '$cmd $csv';
+
+# not sure how to throw keystrokes at this application
+# or how to snag the screen...
+cmd="hecate";  checkCommand "$cmd" 'TERM=xterm "$cmd" "$csv" & kill %1;'
+
+cmd="vimrc";   checkCommand "$cmd" 'ls /etc/profile.d/vim_runtime/*${cmd}*';
+cmd=".tmux";   checkCommand "$cmd" 'ls /etc/skel/${cmd}*';
+cmd="powerline fonts"; checkCommand "$cmd" 'ls /usr/share/fonts/truetype/dejavu/*owerline*';
+cmd="tldr";    checkCommand "$cmd" '$cmd $cmd';
+
+cmd='fzf';     checkCommand "$cmd" '$cmd --version';
+
+# spot-check files copied in
+cmd='nix-nice'; checkCommand "$cmd" 'readlink /opt/$cmd/bin/gethost.sh';
+
+# cleanup
+rm "$csv" "$pdf";
--- container/filesystem/opt/nix-nice/sbin/container-startup.sh
+++ container/filesystem/opt/nix-nice/sbin/container-startup.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail;
 IFS=$'\n\t';
 
 #
 # Linux Container Entry Point / Startup script
 #
-#   * if persisted system files exist, restore them
 #   * create users based on /hosthome directories
 #   * if /home/<user> already exists (e.g. if persisted via a mount), ensure
 #     that the uid is preserved
@@ -13,55 +12,25 @@ IFS=$'\n\t';
 #
 
 
-#
-# ------
-# Function to show usage information if unknown options are specified
-#
-usage() {
-	cat <<EOF >&2
-Invalid flag specified in "$@";
-Usage: $0 [-e] [-p] [-u] [-s]
-	-e to handle environment variables passed in at container startup
-	-p TODO: to restore persisted files, based on persistfs volume mount
-	-u to create users, based on hosthome volume mount
-	-s to start services (SSH, dbus)
-EOF
-	exit 1;
-}
-
-
-#
-# ------
-# Main
-#
-while getopts ":epus" opt
-do
-	case "$opt" in
-		e) ENVIRO=1;;
-		p) PERSIST=1;;
-		u) USERS=1;;
-		s) SERVICES=1;;
-		*) usage "$@";;
-	esac;
-done;
+# if command provided, run it and exit
+if [ "$#" -gt 0 ]
+then
+	eval "$@";
+	exit 0;
+fi;
 
 # Do all the setup and save the output to a log file
 (
 	echo -e "\n============================================================";
 	echo "$(date +'%Y-%m-%d %H:%M:%S %:z (%a, %Z)') Container Startup Script Execution";
-	echo "Full commandline: $@";
-	/opt/nix-nice/sbin/create-links.sh;
-	[ -n "${ENVIRO:-}"  ] && /opt/nix-nice/sbin/environment-setup.sh;
-	[ -n "${PERSIST:-}" ] && echo "No persistent file restore yet"; #/opt/nix-nice/sbin/persistfs-sync.sh;
-	[ -n "${USERS:-}"   ] && /opt/nix-nice/sbin/create-users.sh;
-	if [ -n "${SERVICES:-}" ]
-	then
-		echo "Starting dbus daemon";
-		service dbus start;
-		echo "Starting SSH daemon";
-		service ssh start;
-	fi;
-) 2>&1 | tee -a /var/log/$(basename "$0");
 
-# keep container running after doing all of the above
-exec sleep infinity;
+	/opt/nix-nice/sbin/create-links.sh;
+	/opt/nix-nice/sbin/environment-setup.sh;
+	/opt/nix-nice/sbin/create-users.sh;
+
+	echo "Starting dbus daemon";
+	service dbus start;
+	echo "Starting SSH daemon";
+	service ssh start -D;  # -D => run in the foreground
+
+) 2>&1 | tee -a /var/log/"$(basename "$0")";
